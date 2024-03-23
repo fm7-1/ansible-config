3472bc6754b7603769995533bbeb1be8ab43cea7
Fabricio Mera <55803899+fm7-1@users.noreply.github.com>
2024-03-23 00:37:49 -0500

    Remove comments

--- devenv.nix
+++ devenv.nix
@@ -5,22 +5,11 @@
 
    pkgs.git
    pkgs.glibcLocales
-
  ];
 
   enterShell = ''
     export LC_ALL="C.UTF-8"
-    git --version
   '';
 
-  # https://devenv.sh/languages/
   languages.ansible.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
 }
